f02edfc782c59bb77f14fdc6ae0135661c40f282
Michael Swiderski <michaelski418@gmail.com>
2019-03-22 11:40:13 -0400

    Blank pages made, MainActivity is intended as a starting page.

---
+++ app/src/main/java/com/ginjaninja/dogcatnoisey/DogQuietPage.java
@@ -0,0 +1,13 @@
+package com.ginjaninja.dogcatnoisey;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DogQuietPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dog_quiet_page);
+    }
+}
