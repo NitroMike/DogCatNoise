ed68281f86bd9e845aecdca89f64b788c2005e8b
Vi0let_M0th <43040211+RainbowTomato@users.noreply.github.com>
2019-03-25 10:59:15 -0400

    Update CatNoisePage.java

--- app/src/main/java/com/ginjaninja/dogcatnoisey/CatNoisePage.java
+++ app/src/main/java/com/ginjaninja/dogcatnoisey/CatNoisePage.java
@@ -2,6 +2,7 @@ package com.ginjaninja.dogcatnoisey;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+<<<<<<< HEAD
 //Test
 =======
 >>>>>>> 6ed52567e4ccd492da6182cbf9af5ab539b726c2
