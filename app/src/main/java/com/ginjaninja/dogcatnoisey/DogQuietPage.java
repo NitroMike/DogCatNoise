203405cea499442a4af0058acc1bc82cc13ecea1
karimasaur <48832270+karimasaur@users.noreply.github.com>
2019-03-29 10:43:50 -0400

    Dog Quiet Page Updated

--- app/src/main/java/com/ginjaninja/dogcatnoisey/DogQuietPage.java
+++ app/src/main/java/com/ginjaninja/dogcatnoisey/DogQuietPage.java
@@ -1,7 +1,11 @@
 package com.ginjaninja.dogcatnoisey;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
 //this is karim's page
 public class DogQuietPage extends AppCompatActivity {
 
@@ -9,5 +13,19 @@ public class DogQuietPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dog_quiet_page);
+    }
+    public Button clickbutton1;
+    public void init(){
+        clickbutton1= (Button)findViewById(R.id.button);
+        clickbutton1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent click = new Intent(DogQuietPage.this, DogNoisePage.class);
+
+                startActivity(click);
+
+            }
+        });
     }
 }
