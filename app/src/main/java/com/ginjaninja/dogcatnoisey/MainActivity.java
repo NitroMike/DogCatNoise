6a92e9d993fa1abf379d37865e30d1754ba67665
Michael Swiderski <michaelski418@gmail.com>
2019-04-01 10:20:37 -0400

    Connected main page to dog quiet page.

--- app/src/main/java/com/ginjaninja/dogcatnoisey/MainActivity.java
+++ app/src/main/java/com/ginjaninja/dogcatnoisey/MainActivity.java
@@ -1,12 +1,20 @@
 package com.ginjaninja.dogcatnoisey;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
 //This is Michael's page
 public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onQDClick(View view) {
+        Intent myIntent = new Intent(getBaseContext(),DogQuietPage.class);
+        startActivity(myIntent);
     }
 }
