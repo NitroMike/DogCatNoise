a131b1e97aefbaf8a126ba062950d2977c4eb6d8
JoeMayglothling <48917195+JoeMayglothling@users.noreply.github.com>
2019-04-01 10:04:02 -0400

    Dog barking plus dog barking folder

--- app/src/main/java/com/ginjaninja/dogcatnoisey/DogNoisePage.java
+++ app/src/main/java/com/ginjaninja/dogcatnoisey/DogNoisePage.java
@@ -1,7 +1,11 @@
 package com.ginjaninja.dogcatnoisey;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 //This is Joe's page
 public class DogNoisePage extends AppCompatActivity {
 
@@ -9,5 +13,16 @@ public class DogNoisePage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dog_noise_page);
+
+        final MediaPlayer dogBarkMP = MediaPlayer.create(this, R.raw.dogBark);
+
+        Button playDogBark = (Button) this.findViewById(R.id.dogBark);
+
+        playDogBark.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dogBarkMP.start();
+            }
+        });
     }
 }
