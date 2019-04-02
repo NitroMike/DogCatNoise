074ab43031388fc5ac76d5d7ed4cc03512dd7924
Michael Swiderski <michaelski418@gmail.com>
2019-04-02 10:30:25 -0400

    Revert "Merge remote-tracking branch 'origin/master'"
    This reverts commit f1af683bf2ec8f43a82d62006c831af7dd26b7b2, reversing
    changes made to c7f405e19b48c3adabbcb123a10de10e4d96e299.

--- app/src/main/java/com/ginjaninja/dogcatnoisey/DogQuietPage.java
+++ app/src/main/java/com/ginjaninja/dogcatnoisey/DogQuietPage.java
@@ -14,39 +14,18 @@ public class DogQuietPage extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dog_quiet_page);
     }
-
-    public Button Dog;
-    public Button Back;
-
-    public void GoForward() {
-        Dog = (Button) findViewById(R.id.button);
-        Dog.setOnClickListener(new View.OnClickListener() {
+    public Button clickbutton1;
+    public void init(){
+        clickbutton1= (Button)findViewById(R.id.button);
+        clickbutton1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                Intent FindNoisePage = new Intent(DogQuietPage.this, DogNoisePage.class);
+                Intent click = new Intent(DogQuietPage.this, DogNoisePage.class);
 
-                startActivity(FindNoisePage);
-
-            }
-        });
-    }
-
-    public void GoBackwards() {
-        Back = (Button) findViewById(R.id.button2);
-        Back.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                Intent GoBack = new Intent(DogQuietPage.this, MainActivity.class);
-
-                startActivity(GoBack);
-
+                startActivity(click);
 
             }
         });
     }
 }
-
-
-
