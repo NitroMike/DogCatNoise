c62fbd26b3796f1d198e92bd1d1accab596d1873
Vi0let_M0th <43040211+RainbowTomato@users.noreply.github.com>
2019-04-02 10:36:10 -0400

    All cat noise edits

--- app/src/main/java/com/ginjaninja/dogcatnoisey/CatNoisePage.java
+++ app/src/main/java/com/ginjaninja/dogcatnoisey/CatNoisePage.java
@@ -1,13 +1,29 @@
 package com.ginjaninja.dogcatnoisey;
+//Logan
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-//This is Logan's file
+
 public class CatNoisePage extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cat_noise_page);
+
+        MediaPlayer mp = MediaPlayer.create(CatNoisePage.this, R.raw.cat_meow);
+        mp.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+
+            @Override
+            public void onCompletion(MediaPlayer mp) {
+
+                mp.release();
+            }
+
+        });
+        mp.start();
+
+        }
     }
-}
+
