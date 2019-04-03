3eb678afcd156db301d523e48d3cb39143782e7c
csharak <38789026+csharak@users.noreply.github.com>
2019-04-03 10:17:49 -0400

    can now switch pages after pressing button

--- app/src/main/java/com/ginjaninja/dogcatnoisey/CatQuietPage.java
+++ app/src/main/java/com/ginjaninja/dogcatnoisey/CatQuietPage.java
@@ -1,9 +1,28 @@
 package com.ginjaninja.dogcatnoisey;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 //This is Chris's Page
 public class CatQuietPage extends AppCompatActivity {
+
+    public Button cat_button;
+
+    public void init() {
+        cat_button = (Button) findViewById(R.id.cat_button);
+        cat_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                    Intent toy = new Intent(CatQuietPage.this,CatNoisePage.class);
+
+                init();
+            }
+        });
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
