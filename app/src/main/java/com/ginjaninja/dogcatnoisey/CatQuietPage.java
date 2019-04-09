46beaf678c2f1a3de726660bf3d5cdb09b3be013
csharak <38789026+csharak@users.noreply.github.com>
2019-04-09 10:28:39 -0400

    applied use for back button on cat quiet

--- app/src/main/java/com/ginjaninja/dogcatnoisey/CatQuietPage.java
+++ app/src/main/java/com/ginjaninja/dogcatnoisey/CatQuietPage.java
@@ -10,19 +10,33 @@ import android.widget.Button;
 public class CatQuietPage extends AppCompatActivity {
 
     public Button cat_button;
+    public Button cat_back;
 
 
     public void init() {
         cat_button = (Button) findViewById(R.id.cat_button);
+
         cat_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
 
                     Intent toy = new Intent(CatQuietPage.this,CatNoisePage.class);
 
                     startActivity(toy);
             }
         });
+         cat_back = (Button) findViewById(R.id.backArrow);
+
+         cat_back.setOnClickListener(new View.OnClickListener(){
+             @Override
+             public void onClick(View v) {
+
+                    Intent toy = new Intent(CatQuietPage.this,CatNoisePage.class);
+
+                    startActivity(toy);
+             }
+         });
     }
 
     @Override
