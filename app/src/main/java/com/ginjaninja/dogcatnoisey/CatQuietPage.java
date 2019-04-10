25e13e6ded363231329044734800942a72960c42
csharak <38789026+csharak@users.noreply.github.com>
2019-04-10 10:15:20 -0400

    Fixed the button to go to the next page and to go to the DogQuietPage

--- app/src/main/java/com/ginjaninja/dogcatnoisey/CatQuietPage.java
+++ app/src/main/java/com/ginjaninja/dogcatnoisey/CatQuietPage.java
@@ -9,42 +9,28 @@ import android.widget.Button;
 //This is Chris's Page
 public class CatQuietPage extends AppCompatActivity {
 
-    public Button cat_button;
-    public Button cat_back;
 
-
-    public void init() {
-        cat_button = (Button) findViewById(R.id.cat_button);
-
-        cat_button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-
-                    Intent toy = new Intent(CatQuietPage.this,CatNoisePage.class);
-
-                    startActivity(toy);
-            }
-        });
-         cat_back = (Button) findViewById(R.id.backArrow);
-
-         cat_back.setOnClickListener(new View.OnClickListener(){
-             @Override
-             public void onClick(View v) {
-
-                    Intent toy = new Intent(CatQuietPage.this,CatNoisePage.class);
-
-                    startActivity(toy);
-             }
-         });
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cat_quiet_page);
 
-        init();
 
+
+    }
+
+    public void To_Dog_Page(View view) {
+
+        Intent dog = new Intent(CatQuietPage.this,DogQuietPage.class);
+
+        startActivity(dog);
+    }
+
+    public void To_Cat_Page(View view) {
+
+        Intent cat = new Intent(CatQuietPage.this,CatNoisePage.class);
+
+        startActivity(cat);
     }
 }
