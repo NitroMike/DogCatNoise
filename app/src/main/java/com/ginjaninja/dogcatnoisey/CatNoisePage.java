ec87110d1824e9d672a7d904ddb572518888f25d
Vi0let_M0th <43040211+RainbowTomato@users.noreply.github.com>
2019-04-10 10:30:10 -0400

    4/10/2019 Half mark

--- app/src/main/java/com/ginjaninja/dogcatnoisey/CatNoisePage.java
+++ app/src/main/java/com/ginjaninja/dogcatnoisey/CatNoisePage.java
@@ -18,14 +18,12 @@ public class CatNoisePage extends AppCompatActivity {
         setContentView(R.layout.activity_cat_noise_page);
 
         final int random = new Random().nextInt(7) + 1; // [0, 7] => [1, 8]
-
         final MediaPlayer catMeowMP = MediaPlayer.create(this, R.raw.cat_meow);
-
         final MediaPlayer catHissMP = MediaPlayer.create(this, R.raw.cat_earrape);
 
         Button playCatMeow = this.findViewById(R.id.MeowCatButton);
-
         Button backArrow = this.findViewById(R.id.backbutton);
+        Button toLoudDog = this.findViewById(R.id.meow_woof_btn);
 
         if (random == 7) {
             playCatMeow.setBackgroundResource(R.drawable.angry_cat);
@@ -56,59 +54,16 @@ public class CatNoisePage extends AppCompatActivity {
                 startActivity(goBack);
             }
         });
-    }
-}
-
-/*public class CatNoisePage extends AppCompatActivity {
-
-    final MediaPlayer cat_Meow = MediaPlayer.create(this, R.raw.dogbark);
-
-    final MediaPlayer cat_earrape = MediaPlayer.create(this, R.raw.cat_earrape);
-
-    Button backArrow = this.findViewById(R.id.backbutton);
-
-    Button MeowCatButton = this.findViewById(R.id.MeowCatButton);
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_cat_noise_page);
-    }
-
-    public void Cat_Button_Clicking(View view) {
-        final int random = new Random().nextInt(7) + 1; // [0, 7] => [1, 8]
-
-        if (random == 7)
-        {
-            MeowCatButton.setBackgroundResource(R.drawable.alphamalewolf);
-
-            MeowCatButton.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    cat_earrape.start();
-                }
-            });
-        }
-        if (random != 7)
-        {
-            MeowCatButton.setBackgroundResource(R.drawable.angry_cat);
-
-            MeowCatButton.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    cat_Meow.start();
-                }
-            });
-        }
-        backArrow.setOnClickListener(new View.OnClickListener() {
+        toLoudDog.setOnClickListener(new View.OnClickListener(){
             @Override
-            public void onClick(View v) {
-                cat_Meow.stop();
-                cat_earrape.stop();
-                Intent goBack = new Intent(getBaseContext(), DogQuietPage.class);
+            public void onClick(View v){
+                catMeowMP.stop();
+                catHissMP.stop();
+                Intent goBack = new Intent(getBaseContext(), DogNoisePage.class);
                 startActivity(goBack);
             }
         });
+
     }
 }
-*/
+
