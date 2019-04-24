189c930ba22a4cab2cbb87606d4b59895bb818be
karimasaur <48832270+karimasaur@users.noreply.github.com>
2019-04-24 10:55:10 -0400

    Dog Quiet Updated Once Again

--- app/src/main/java/com/ginjaninja/dogcatnoisey/DogQuietPage.java
+++ app/src/main/java/com/ginjaninja/dogcatnoisey/DogQuietPage.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 
 //this is Karim's page
 public class DogQuietPage extends AppCompatActivity {
@@ -12,6 +13,16 @@ public class DogQuietPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dog_quiet_page);
+
+        Button button =  this.findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent goBack = new Intent(getBaseContext(), DogQuietPage.class);
+                startActivity(goBack);
+            }
+        });
     }
 //    public Button clickbutton1;
 //
@@ -27,10 +38,15 @@ public class DogQuietPage extends AppCompatActivity {
 //            }
 //        });
 //    }
-    public void onQDClick(View view) {
-        Intent findNoise = new Intent(getBaseContext(),DogNoisePage.class);
-        startActivity(findNoise);
-    }
+//    public void onQDClick(View view) {
+//        Intent findNoise = new Intent(getBaseContext(),DogNoisePage.class);
+//        startActivity(findNoise);
+//    }
+
+
+
+
+
     public void onQCClick(View view) {
         Intent goBack = new Intent(getBaseContext(),MainActivity.class);
         startActivity(goBack);
